9c7fbb6a38b8d89e6cd50ef3daa29dd7b9405511
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-12 17:00:16 +0900

    Create Dto03.java

---
+++ src/main/java/com/example/demo/domain/Dto03.java
@@ -0,0 +1,16 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dto03 {
+	private String name;
+	private int age;
+	
+}
