0242294253b8b3b036cb7dc167f0881f8c25a9e3
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-12 17:00:21 +0900

    Create Dto01.java

---
+++ src/main/java/com/example/demo/domain/Dto01.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class Dto01 {
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@RequestMapping("link8")
+	public void method8(Model model) {
+		Dto01 o1 = new Dto01();
+		o1.setName("박지성");
+		o1.setAge(40);
+		
+		model.addAttribute("player", o1);
+	}
+	
+}
