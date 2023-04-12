337a1346190816e9223997565b5dec05f9540db8
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-12 17:00:18 +0900

    Create Dto02.java

---
+++ src/main/java/com/example/demo/domain/Dto02.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+public class Dto02 {
+	private String model;
+	private int price;
+	private String company;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+}
