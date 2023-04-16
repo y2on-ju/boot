7b91ed30a6db23064dbf5c4a907409caf349ad1f
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-16 17:19:13 +0900

    Create Customer.java

---
+++ src/main/java/com/example/demo/domain/Customer.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+@Data
+public class Customer {
+	private int id;
+	private String name;
+	private String address;
+	private String contactName;
+}
