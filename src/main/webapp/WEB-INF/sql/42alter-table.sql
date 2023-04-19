c1dcf9694e7f8ff22377d91fb8402957bf48191d
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-19 17:37:26 +0900

    Create 42alter-table.sql

---
+++ src/main/webapp/WEB-INF/sql/42alter-table.sql
@@ -0,0 +1,31 @@
+-- ALTER TABLE MODIFY COLUMN : 제약사항 추가
+CREATE TABLE MyTable28 (
+	col1 INT,
+    col2 INT,
+	col3 INT,
+    col4 INT
+);
+DESC MyTable28;
+ALTER TABLE MyTable28 
+MODIFY COLUMN col1 INT UNIQUE;
+ALTER TABLE MyTable28
+MODIFY COLUMN col2 INT NOT NULL;
+-- col3에 default 0 제약사항 추가
+ALTER TABLE MyTable28 
+MODIFY COLUMN col3 INT DEFAULT 0;
+-- col4에 not null unique default 100 제약사항 추가
+ALTER TABLE MyTable28
+MODIFY COLUMN col4 INT NOT NULL UNIQUE DEFAULT 100;
+DESC MyTable28;
+
+-- ADD PRIMARY KEY : PRIMARY KEY 추가
+CREATE TABLE MyTable29 (
+	col1 INT,
+    col2 INT,
+    col3 INT
+);
+ALTER TABLE MyTable29 
+ADD PRIMARY KEY (col1, col2);
+DESC MyTable29;
+
+
