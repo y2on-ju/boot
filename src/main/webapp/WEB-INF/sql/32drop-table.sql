26e7cf1f65dbcd386446af0708212168be37e469
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-19 17:37:56 +0900

    Create 32drop-table.sql

---
+++ src/main/webapp/WEB-INF/sql/32drop-table.sql
@@ -0,0 +1,17 @@
+USE w3schools;
+
+-- DROP TABLE : 테이블 삭제 (주의 필요!!!!!!!!!!!!)
+
+DESC MyTable01;
+
+DROP TABLE MyTable01; -- 주의!!!!!!
+
+-- 연습 : MyTable02, MyTable03 테이블 drop
+DROP TABLE MyTable02;
+DROP TABLE MyTable02;
+
+-- TRUNCATE : 테이블 유지, 데이터 삭제 (복구 안됨)
+SELECT * FROM MyTable04;
+DESC MyTable04;
+TRUNCATE MyTable04; -- 주의 !!!!!!!
+
