098406b87e90aa3ec4e855b7b2202ce56cdea754
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-21 16:47:18 +0900

    Create Dto10.java

---
+++ src/main/java/com/example/demo/domain/Dto10.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+@Data
+public class Dto10 {
+	private Integer age; // INT
+	private String name; // VARCHAR
+	private Double score; // DEC
+}
