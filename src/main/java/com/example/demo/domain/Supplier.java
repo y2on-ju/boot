b2b4f4564bc63249e298bcd06553a460d2a21c8c
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-02 20:44:52 +0900

    Update Supplier.java

--- src/main/java/com/example/demo/domain/Supplier.java
+++ src/main/java/com/example/demo/domain/Supplier.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain;
 
+import java.util.*;
+
 import lombok.*;
 
 @Data
@@ -12,4 +14,7 @@ public class Supplier {
 	private String country;
 	private String postalCode;
 	private String phone;
+	
+	private List<Product> products;
 }
+
