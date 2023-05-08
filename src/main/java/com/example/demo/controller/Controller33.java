d9e7f27ad3754db6b0225b60c2715c1e38564730
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 17:43:52 +0900

    Create Controller33.java

---
+++ src/main/java/com/example/demo/controller/Controller33.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("sub33")
+public class Controller33 {
+
+	@GetMapping("mylogin")
+	public void loginForm() {
+		
+	}
+}
