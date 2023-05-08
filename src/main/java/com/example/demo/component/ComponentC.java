b9fbbe854760e14f6af71e60b92a2c9f8c041dc8
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 17:44:07 +0900

    Create ComponentC.java

---
+++ src/main/java/com/example/demo/component/ComponentC.java
@@ -0,0 +1,13 @@
+package com.example.demo.component;
+
+public class ComponentC {
+	private ComponentB com;
+
+	public void setCom(ComponentB com) {
+		this.com = com;
+	}
+
+	public ComponentB getCom() {
+		return com;
+	}
+}
