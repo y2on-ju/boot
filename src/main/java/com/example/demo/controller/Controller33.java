da61bc5e2c718b0ae9a6c4b52f19f9c2abb93de3
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-09 16:54:47 +0900

    Update Controller33.java

--- src/main/java/com/example/demo/controller/Controller33.java
+++ src/main/java/com/example/demo/controller/Controller33.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.security.access.prepost.*;
+import org.springframework.security.core.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,4 +13,29 @@ public class Controller33 {
 	public void loginForm() {
 		
 	}
+	
+	@GetMapping("loginSuccess")
+	public void loginSucess() {
+		
+	}
+	
+	@GetMapping("viewAuth")
+	public void viewAuth(Authentication authentication) {
+		
+		System.out.println("로그인 정보 확인 하기");
+		
+		System.out.println(authentication);
+		System.out.println(authentication.getName());
+	}
+	
+	@GetMapping("customCheck")
+	@PreAuthorize("@securityUtil.checkBoardWriter(authentication, #id)")
+	public void customCheck(String id) {
+		System.out.println("customCheck 메소드 실행중.");
+	}
 }
+
+
+
+
+
