5249b73e65634f7b2089241ba7d858928b861750
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-10 17:16:35 +0900

    Update MyConfig2.java

--- src/main/java/com/example/demo/config/MyConfig2.java
+++ src/main/java/com/example/demo/config/MyConfig2.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.method.configuration.*;
 import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.*;
@@ -45,12 +46,18 @@ public class MyConfig2 {
 		UserDetails user1 = User.builder()
 				.username("user1")
 				.password(pw1)
-				.authorities(List.of())
+				.authorities(List.of(
+						new SimpleGrantedAuthority("admin"),
+						new SimpleGrantedAuthority("manager")
+						))
 				.build();
 		UserDetails user2 = User.builder()
 				.username("user2")
 				.password(pw2)
-				.authorities(List.of())
+				.authorities(List.of(
+						new SimpleGrantedAuthority("manager"),
+						new SimpleGrantedAuthority("user")
+						))
 				.build();
 
 		return new InMemoryUserDetailsManager(user1, user2);
