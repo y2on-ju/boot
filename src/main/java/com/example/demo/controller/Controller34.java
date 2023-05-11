6d207dd8f622463d0a270713b56f09193ae14b44
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-11 16:56:18 +0900

    Update Controller34.java

--- src/main/java/com/example/demo/controller/Controller34.java
+++ src/main/java/com/example/demo/controller/Controller34.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.*;
+
 @Controller
 @RequestMapping("sub34")
 public class Controller34 {
@@ -18,6 +20,61 @@ public class Controller34 {
 		System.out.println("헬로 ajax");
 		
 		return "첫번째 응답 데이터";
+	}
+	
+	@GetMapping("link2")
+	public void method2() {
+		System.out.println("get 요청 link2");
+	}
+	
+	@GetMapping("link3")
+	public void method3(@RequestParam("name") String name) {
+		System.out.println("link3 메소드 일함");
+		System.out.println("name:" + name);
+	}
+	
+	@GetMapping("link4")
+	public void method4(String address) {
+		System.out.println("link4 메소드 일함");
+		System.out.println("address:" + address);
+	}
+	
+	// /sub34/link5?name=손&age=22
+	@GetMapping("link5")
+	public void method5(String name, Integer age) {
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+	}
+	
+	@GetMapping("link6")
+	public void method6(String food, Double price) {
+		System.out.println("food:" + food);
+		System.out.println("price:" + price);
+	}
+	
+	@Data
+	static class Dto1 {
+		private String address;
+		private String name;
+		private Integer age;
+	}
+	
+	// /sub34/link7?address=seoul&name=park&age=33
+	@GetMapping("link7")
+	public void method7(@ModelAttribute Dto1 dto) {
+		System.out.println(dto);
+	}
+	
+	@Data
+	static class Dto2 {
+		private Double price;
+		private String product;
+		private String category;
+	}
+	@GetMapping("link8")
+	public void method8(Dto2 dto) {
+		System.out.println(dto);
+//		service.add(dto);
 	}
 }
 
