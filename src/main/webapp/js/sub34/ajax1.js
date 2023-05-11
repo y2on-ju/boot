c8bc21c790dc32f662d843b075ff768d67ff1c97
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-11 16:56:06 +0900

    Update ajax1.js

--- src/main/webapp/js/sub34/ajax1.js
+++ src/main/webapp/js/sub34/ajax1.js
@@ -1,3 +1,50 @@
 $("#button1").click(function() {
 	$.ajax("/sub34/link1");
 });
+
+$("#button2").click(function() {
+	$.ajax("/sub34/link2");
+});
+
+$("#button3").click(function() {
+	$.ajax("/sub34/link3?name=son");
+});
+
+$("#button4").click(function() {
+	$.ajax("/sub34/link4?address=서울");
+})
+
+$("#button5").click(function() {
+	$.ajax("/sub34/link5?name=손&age=22");
+});
+
+$("#button6").click(function() {
+	$.ajax("/sub34/link6?food=pizza&price=3.14")
+});
+
+$("#button7").click(function() {
+	$.ajax("/sub34/link7?address=seoul&name=park&age=33");
+})
+
+$("#button8").click(function() {
+	$.ajax("/sub34/link8?price=3.14&product=파이&category=음식");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
